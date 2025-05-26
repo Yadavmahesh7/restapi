be4bb8fb6ab8095e68d47239a97cbd68dc75b17e
Yadavmahesh7 <mahi224224@gmail.com>
2025-05-26 11:50:22 +0530

    initial commit

---
+++ src/test/java/com/yadavmahesh/restapi/RestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yadavmahesh.restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
